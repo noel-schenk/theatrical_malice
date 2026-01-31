01a3b6bc00ec7f26f1dfd303a0f66cc07e798fc0
Noel Schenk <schenknoel@gmail.com>
2026-01-31 23:10:14 +0100

    add hint

--- src/components/PlayerHints/PlayerHints.tsx
+++ src/components/PlayerHints/PlayerHints.tsx
@@ -1,13 +1,21 @@
 import type { MaskDefinition } from '@/models/MaskDefinition'
 import { mainState } from '@/state/mainState'
 
-import type { FC } from 'react'
+import { type FC, useState } from 'react'
 
 import { cloneDeep, isNil } from 'lodash-es'
 import { useSnapshot } from 'valtio'
 
 import Mask from '../Mask/Mask'
+import MaskDescription from '../MaskDescription/MaskDescription'
 import { Avatar, AvatarFallback } from '../ui/avatar'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '../ui/dialog'
 import { PlayerHintsWrapper } from './PlayerHints.styled'
 
 interface PlayerHintsProps {}
@@ -15,12 +23,23 @@ interface PlayerHintsProps {}
 const PlayerHints: FC<PlayerHintsProps> = () => {
   const mainSnap = useSnapshot(mainState)
 
+  const [selectedPlayerHint, setSelectedPlayerHint] = useState<MaskDefinition>()
+
   return (
     <PlayerHintsWrapper>
       {mainSnap.players
         .filter(player => !isNil(player.playerUUID) && player.found === false)
         .map((player, index) => (
-          <Avatar key={index}>
+          <Avatar
+            key={index}
+            onClick={() => {
+              setSelectedPlayerHint(
+                cloneDeep(
+                  player.advancedMaskProperty.maskDefinition
+                ) as MaskDefinition
+              )
+            }}
+          >
             <AvatarFallback>
               <div style={{ transform: 'scale(0.2)' }}>
                 <Mask
@@ -34,6 +53,23 @@ const PlayerHints: FC<PlayerHintsProps> = () => {
             </AvatarFallback>
           </Avatar>
         ))}
+      <Dialog
+        open={!isNil(selectedPlayerHint)}
+        onOpenChange={open => {
+          if (!open) setSelectedPlayerHint(undefined)
+        }}
+      >
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>You want a hint? We got you!</DialogTitle>
+            <DialogDescription className="p-4 py-8">
+              {!isNil(selectedPlayerHint) && (
+                <MaskDescription maskDefinition={selectedPlayerHint} />
+              )}
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </PlayerHintsWrapper>
   )
 }
