4fc7d96260710e6d7b4cf56e8f1e4bb4c30d9e15
LarsYoung <lars.jung90@gmail.com>
2026-01-31 14:26:12 +0100

    Add face and more masks

--- src/components/Mask/Mask.tsx
+++ src/components/Mask/Mask.tsx
@@ -2,6 +2,7 @@ import type { FC } from "react";
 import { MaskWrapper } from "./Mask.styled";
 import type { MaskDefinition } from "../../models/MaskDefinition";
 import MaskFeature from "../MaskFeature/MaskFeature";
+import Face from '../../assets/face/Face.png';
 
 interface MaskProps {
   maskDefinition: MaskDefinition;
@@ -9,6 +10,7 @@ interface MaskProps {
 
 const Mask: FC<MaskProps> = (props) => (
   <MaskWrapper>
+    <img src={Face} alt=""></img>
     <img src={props.maskDefinition.url} alt=""></img>
     {props.maskDefinition.features.map((feature) => (
       <MaskFeature feature={feature}></MaskFeature>
